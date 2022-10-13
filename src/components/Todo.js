cf6c84f5e06a34226bb0a08eac7826834369cc25
prathamesh <prathameshkulkarni952@gmail.com>
2022-10-13 23:51:42 +0530

    complete btn

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,12 +5,21 @@ export default function Todo({Todos,setTodos,text,todo}) {
     const deletHandler = () => {
         setTodos(Todos.filter((el) => el.id !== todo.id));
         // console.log(todo)
-
+    }
+    const completeHandler = () => {
+        setTodos(Todos.map(item => {
+            if(item.id === todo.id){
+                return {
+                    ...item, completed: !item.completed
+                }
+            }
+            return item
+        }))
     }
   return (
     <div className="todo">
-        <li className="todo-item">{text}</li>
-        <button  className="complete-btn">
+        <li className={`todo-item ${todo.completed ? "completed" : ''}`}>{text}</li>
+        <button  onClick={completeHandler} className="complete-btn">
             <i className="fas fa-check"></i>
         </button>
         <button onClick={deletHandler} className="trash-btn">
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -23,13 +23,7 @@ export default function TodoForm({InputText,setInputText,Todos,setTodos}) {
       <button className="todo-button" type="submit" onClick={submitHandler}>
         <i className="fas fa-plus-square"></i>
       </button>
-      <div className="select">
-        <select name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
+     
     </form>
     </div>
   )
